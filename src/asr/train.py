5178e41d4a090fa3ea22dec4af50e807d34f6996
kayc <ghstkayc@gmail.com>
2025-06-18 00:08:03 +0545

    add: matmul precision

--- src/asr/train.py
+++ src/asr/train.py
@@ -17,7 +17,8 @@ from asr.dataset import SpeechDataModule
 
 def main(args):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
+    torch.set_float32_matmul_precision('medium')
+    
     # Prepare dataset
     data_module = SpeechDataModule(
         batch_size=args.batch_size,
